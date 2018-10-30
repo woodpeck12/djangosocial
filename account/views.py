abf7190b346dd7a85906bee09255119fc75dd445
woodpeck <woodpeck1@gmail.com>
2018-10-30 23:38:37 +1100

    login form partial create

--- account/views.py
+++ account/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+#customised login
+from .forms import LoginForm
 
 # Create your views here.
+
+#def user_login(request):
+
