5b4018f3fcf67d2e292c017e319a515453726cae
woodpeck <woodpeck1@gmail.com>
2018-10-31 23:31:34 +1100

    progress building login form and process

--- account/views.py
+++ account/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse  #woodpeck
+from django.contrib.auth import authenticate, login
+
 
 #customised login
 from .forms import LoginForm
 
 # Create your views here.
 
-#def user_login(request):
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+
+            user = authenticate(username=cleaned_data['username'],
+                                password=cleaned_data['password'])
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('logined')
+                else:
+                    return HttpResponse('suspended')
+            else:
+                return HttpResponse('what are you doing')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form':form})
+
 
--- djangosocial/settings.py
+++ djangosocial/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account',
-    'bookmarks',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
